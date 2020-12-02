a2c196e7ab0b1841c8fbe6d2483d2b7ad8a4ddd5
spaceC00kie <kirstie317@gmail.com>
2020-12-02 13:19:04 -0500

    strings

---
+++ traversyjs04.js
@@ -0,0 +1,12 @@
+// Strings
+
+const name = "Hubert";
+const age = 159;
+
+// Concatenation    (this is the older way)
+console.log("My name is " + name + " and I am " + age);
+
+// Template String
+const hello = `My name is ${name} and I am ${age}`;
+
+console.log(hello);
